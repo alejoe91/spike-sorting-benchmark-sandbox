acda53cfc48bc1ad9f24b28f3d59744237e8fbfd
olivier <olivier.winter@hotmail.fr>
2024-02-28 15:40:21 +0000

    examples Olivier

--- compute_residuals_spike_interface.py
+++ compute_residuals_spike_interface.py
@@ -1,9 +1,73 @@
 # %%
+from pathlib import Path
 from one.api import ONE
+import numpy as np
 from brainbox.io.one import SpikeSortingLoader
-from ibldsp.voltage import destripe
-from viewephys.gui import viewephys
-from spikeinterface.extractors.iblextractors import IblSortingExtractor, IblRecordingExtractor
+
+from spikeinterface.extractors.cbin_ibl import CompressedBinaryIblExtractor
+
+from spikeinterface.extractors import read_alf_sorting
+import spikeinterface.full as si
+
+from spikeinterface.core import BaseRecording
+from spikeinterface.core.generate import InjectTemplatesRecording
+import spikeinterface.preprocessing as spre
+import spikeinterface.postprocessing as spost
+
+
+def compute_residuals(waveform_extractor, with_scaling=True, overwrite=False, sparsity=None,  **scaling_kwargs):
+    """
+    Compute residuals for a given recording and sorting.
+
+    Parameters
+    ----------
+    waveform_extractor: si.WaveformExtractor
+        The waveform extractor
+    with_scaling: bool
+        If True, use amplitude scaling
+
+    Returns
+    -------
+    residuals: BaseRecording
+        The recording after residual computation
+    """
+    if with_scaling:
+        # compute amplitude scalings
+        if waveform_extractor.has_extension("amplitude_scalings") and not overwrite:
+            asc = waveform_extractor.load_extension("amplitude_scalings")
+            amp_scalings= asc.get_data()
+        else:
+            amp_scalings = spost.compute_amplitude_scalings(waveform_extractor, **scaling_kwargs)
+    else:
+        amp_scalings = None
+
+    templates = waveform_extractor.get_all_templates()
+    if waveform_extractor.return_scaled:
+        templates = templates / waveform_extractor.recording.get_channel_gains()[:, None]
+    if sparsity:
+        templates_full = np.zeros_like(templates)
+        for i, (unit, sparsity_indices) in enumerate(sparsity.unit_id_to_channel_indices.items()):
+            templates_full[i, :, sparsity_indices] = templates[i, :, sparsity_indices]
+        templates = templates_full
+    templates = templates.astype("float32")
+
+    recording_float = spre.astype(waveform_extractor.recording, dtype="float32")
+
+    residuals = InjectTemplatesRecording(
+        sorting=waveform_extractor.sorting,
+        parent_recording=recording_float,
+        amplitude_factor=amp_scalings,
+        templates=-templates
+    )
+    convolved = InjectTemplatesRecording(
+        sorting=waveform_extractor.sorting,
+        templates=templates,
+        amplitude_factor=amp_scalings,
+    )
+    convolved = convolved.rename_channels(residuals.channel_ids)
+
+    return residuals, convolved
+
 
 one = ONE(base_url='https://openalyx.internationalbrainlab.org')
 
@@ -21,35 +85,53 @@ benchmark_pids = ['1a276285-8b0e-4cc9-9f0a-a3a002978724',
                   'eebcaf65-7fa4-4118-869d-a084e84530e2',
                   'fe380793-8035-414e-b000-09bfe5ece92a']
 
-# %%
-[600, 1200]
-# Select a PID
+PATH_CBIN = Path("/datadisk/Data/neuropixel/spike_sorting/benchmarks")
+PATH_SPIKE_INTERFACE = Path("/datadisk/Data/neuropixel/spike_sorting/benchmarks/spikeinterface")
+PATH_SPIKE_INTERFACE.mkdir(exist_ok=True, parents=True)
+
 for pid in benchmark_pids:
-    sorting = IblSortingExtractor(pid=pid, one=one)
-    recording = IblRecordingExtractor(session=sorting.ssl.eid, stream_name=f"{sorting.ssl.pname}.ap", one=one)
+    print(pid)
+    if PATH_SPIKE_INTERFACE.joinpath(f"{pid}_original.npy").exists():
+        continue
+    recording = CompressedBinaryIblExtractor(cbin_file=PATH_CBIN.joinpath(f"{pid}.ap.cbin"))
+    sorting = read_alf_sorting(PATH_CBIN.joinpath(pid, '1.5.0', 'alf'), sampling_frequency=recording.sampling_frequency)
+    print(sorting)
 
-    recording_slice = recording.frame_slice(start_frame=700 * 30_000, end_frame=701 * 30_000)
+    print(f'{pid} pre-processing')
+    file_preproc = PATH_SPIKE_INTERFACE / f"{pid}_preprocessed.json"
+    if file_preproc.exists():
+        recording_clean = si.load_extractor(file_preproc)
+    else:
+        recording_processed = si.phase_shift(recording)
+        recording_processed = si.highpass_filter(recording_processed)
+        bad_channel_ids, bad_channel_labels = si.detect_bad_channels(recording_processed)
+        recording_clean = recording_processed.remove_channels(bad_channel_ids)
+        recording_clean = si.common_reference(recording_clean)
+        recording_clean.dump_to_json(PATH_SPIKE_INTERFACE / f"{pid}_preprocessed.json")
 
+    print(recording_clean)
+    folder_waveforms = PATH_SPIKE_INTERFACE.joinpath(f"waveforms_{pid}")
+    # Extract waveforms
+    if folder_waveforms.exists():
+        we = si.load_waveforms(folder_waveforms)
+    else:
+        we = si.extract_waveforms(recording_clean, sorting, folder=folder_waveforms,
+                                  overwrite=True, return_scaled=False, n_jobs=.7, sparse=False, progress_bar=True,
+                                  max_spikes_per_unit=200)
+    print(we)
+    print('compute sparsity')
+    sparsity = si.compute_sparsity(we, method='radius', radius_um=144)
+    print('compute residuals')
+    residual, convolved = compute_residuals(we, with_scaling=False, sparsity=sparsity)
 
+    start_frame = int(200 * 30_000)
+    end_frame = int(start_frame + (.1 * 30_000))
 
-## %
-import spikeinterface as si
-import spikeinterface.widgets as sw
-we = si.extract_waveforms(recording_slice, sorting, folder="my_folder", n_jobs=-1)
+    vclean = recording_clean.get_traces(start_frame=start_frame, end_frame=end_frame).T
+    vres = residual.get_traces(start_frame=start_frame, end_frame=end_frame).T
+    vconvolved = convolved.get_traces(start_frame=start_frame, end_frame=end_frame).T
+    np.save(PATH_SPIKE_INTERFACE / f"{pid}_original.npy", vclean)
+    np.save(PATH_SPIKE_INTERFACE / f"{pid}_residual.npy", vres)
+    np.save(PATH_SPIKE_INTERFACE / f"{pid}_spikes_model.npy", vconvolved)
 
-from utils import compute_residuals
-# %%
-# View data
-# %gui qt
-
-v_raw = viewephys(raw_ap, fs=sr_ap.fs)
-v_des = viewephys(destriped, fs=sr_ap.fs)
-
-##
-# Get the spikes time (in samples), extract raw data and make an average
-clu_id = 0
-spike_idx = spikes['cluster'] == clu_id
-spike_samples = spikes['samples'][spike_idx]
-
-# Use euclidian distance to get N nearest channels
 
